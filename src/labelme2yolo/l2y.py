78137ee9a06e873687314479556a007ef609b783
John Chain <jchain3@gmail.com>
2023-01-07 15:08:39 -0500

    switch back to png

--- src/labelme2yolo/l2y.py
+++ src/labelme2yolo/l2y.py
@@ -316,13 +316,14 @@ class Labelme2YOLO(object):
                 f.write(yolo_obj_line)
 
     def _save_yolo_image(self, json_data, json_name, image_dir_path, target_dir):
-        # img_name = json_name.replace('.json', '.png')
-        img_name = json_name.replace('.json', '.jpg')
+        img_name = json_name.replace('.json', '.png')
+        # img_name = json_name.replace('.json', '.jpg')
         img_path = os.path.join(image_dir_path, target_dir, img_name)
 
         if not os.path.exists(img_path):
             img = img_b64_to_arr(json_data['imageData'])
             PIL.Image.fromarray(img).save(img_path)
+            # PIL.Image.fromarray(img).save(img_path, quality=95)
 
         return img_path
 
